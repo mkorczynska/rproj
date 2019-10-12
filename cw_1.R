e5a0fe4e18daba940cfc6f129bfe34185a431c30
mkorczynska <k.martyna95@gmail.com>
2019-10-12 23:31:55 +0200

    .

--- cw_1.R
+++ cw_1.R
@@ -1,41 +1,58 @@
+setwd("C://Users/HP ENVY/Documents/R/Projects/rproj")
+
 ####Zadanie 1
-dane<-read.csv2("C://Users/HP ENVY/Documents/R/Projects/rproj/market.csv", sep =",", header=T)
-show(dane)
-ilosc<-nrow(dane)
-suma<-sum(dane[,5])
+dane<-read.csv2("market.csv", sep =",", header=T)
+quantity<-nrow(dane)
+
+sum(dane[,5])
+
+cena<-as.numeric(dane$cena)
+ilosc<-as.numeric(dane$cena)
+suma<-sum(cena*ilosc)
 
 tab<-table(dane[,3])
 tab_df<-as.data.frame(tab)
 sort(tab, decreasing=TRUE)[1:3]
 
-#agg = aggregate(dane, by = list(dane$nazwa, dane$cena), FUN=mean)
-tab<-table(dane[,3])
-tab_df<-as.data.frame(tab)
+#d
 #-------------
 
 ####Zadanie 2
-crime_data<-read.table(file = 'H://R-zaawansowany/lab_R/crim_just_sex.tsv', sep = '\t', header = TRUE)
-crime_data[crime_data==":"]<-NA
-crime_data[which(crime_data[,2]==";")]<-""
+crime_data<-read.table(file = 'crim_just_sex.tsv', sep = '\t', header = TRUE)
+any(is.na(crime_data))
+
+#b
+#c
+#d
+#e
 #-------------
 
-####Zadanie 3
-zloto<-read.csv2("H://R-zaawansowany/lab_R/zloto.csv", sep =";", header=T)
-daty<-seq(as.Date("2017/02/02"), as.Date("2018/11/20"), by="day")
-zloto[,1]<-daty
+####Zadanie 3####
+zloto<-read.csv2("zloto.csv", sep =";", header=T, dec = ".")
+length(zloto[,1])
+dates_3<-seq(as.Date("2017/02/02"), as.Date("2018/11/20"), length.out = 488)
+zloto[,1]<-dates_3
+
 head(zloto)
 tail(zloto, 10)
+
+month<-months(zloto[,1])
+year <- format(zloto[,1],format="%y")
+aggregate(zloto$Zamkniecie~month+year, zloto, mean)
 #--------------
 
 ####Zadanie 4
-wig20<-read.csv2("H://R-zaawansowany/lab_R/wig20.csv", sep =";", header=T)
-wig20<-cbind(wig20$Data, wig20$Zamkniecie)
-szereg<-ts(wig20)
-plot(szereg)
+wig20<-read.csv2("wig20.csv", sep =";", header=T, dec = ".")
+dates_4<-seq(as.Date("2014/01/31"), as.Date("2018/10/31"), by="month")
+wig20$Data<-dates_4
+wig<-cbind(wig20$Data, wig20$Zamkniecie)
+series<-ts(wig20)
+plot(series)
 #--------------
 
 ####Zadanie 5
-
+#a
+#b
 #--------------
 
 ####Zadanie 6
@@ -43,8 +60,11 @@ install.packages("quantmod")
 library(quantmod)
 
 getSymbols(c("^GSPC","FB","^IXIC","AAPL","CSCO"), from="2016-01-02", to="2018-01-02") 
+month<-months(zloto[,1])
+year <- format(zloto[,1],format="%y")
+aggregate(zloto$Zamkniecie~month+year, zloto, mean)
 
-
+cor(AAPL, CSCO)
 
 
 
