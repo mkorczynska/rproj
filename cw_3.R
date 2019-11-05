6c08cb1db18f921445d45c2444c985ed420fd740
mkorczynska <k.martyna95@gmail.com>
2019-11-05 23:49:25 +0100

    .

--- cw_3.R
+++ cw_3.R
@@ -64,8 +64,25 @@ myNorm(vect_2)
 
 
 #zadanie 4
+myCorr<-function(x, y){
+  if(length(x)==length(y)){
+    list( pearson<-cor(x, y, method = "pearson"),
+          kendall<-cor(x, y, method = "kendall"),
+          spearman<-cor(x, y, method = "spearman")
+          )
+  }
+  else{
+    print("blad")
+  }
+  
+}
 
+x<-runif(100, min = 0, max = 5)
+e<-rnorm(100)
 
+y<-x+e
+
+corrs<-myCorr(x, y)
 
 #zadanie 5
 myStats<-function(x, p){
@@ -92,9 +109,52 @@ myStats(los, 0)
 
 
 #zadanie 6
-myFun<-function(x){
-  y=10*sin(1.5*x)*cos(0.5*x)+(1/2)*sqrt(abs(x))
-  return(y)
+install.packages("rootSolve")
+library(rootSolve)
+myFun=function(x){
+  10*sin(1.5*x)*cos(0.5*x)+(1/2)*sqrt(abs(x))
 }
 
-myFun(1)
+#a
+ur67<-uniroot(myFun, c(6, 7))
+ur12<-uniroot(myFun, c(1, 2))
+ur55<-uniroot(myFun, c(-5, 5))
+
+#b
+urs33<-uniroot.all(myFun, c(-3,3))
+
+plot(myFun, from = -5, to=10, type = "l", ylab = "f(x)", main="10*sin(1.5*x)*cos(0.5*x)+(1/2)*sqrt(abs(x))", cex.main=0.75)
+
+#zadanie 7
+myLin<-function(x){
+  c(F1 = 2*x[1] + x[2] + 2,
+    F2 = x[1] + 2 + x[2] - 2 + x[3] - 1,
+    F3 = 2 * x[1] + x[2] - x[3] + 3 )
+}
+
+solution_Lin<- multiroot(myLin, c(0, 0, 0))
+
+#zadanie 8
+myNonLin<-function(x){
+  c(F1 = 2*x[1] + x[2]^2 -2*x[3] - 2,
+    F2 = x[1]^2 + 2*x[2] - 2*x[3] - 3,
+    F3 = 2*x[1] + x[2] - x[3] - 3 )
+}
+
+solution_nonLin<- multiroot(myNonLin, c(0, 0, 0), useFortran = FALSE)
+
+#zadanie 9
+install.packages("httr")
+install.packages("XML")
+library(httr)
+library(XML)
+myDane<-function(url){
+  r <- GET(url)
+  doc <- readHTMLTable(
+    doc=content(r, "text"))
+}
+
+tab<-myDane("https://pl.wikipedia.org/wiki/Najwi%C4%99ksze_przedsi%C4%99biorstwa_%C5%9Bwiata")
+tab<-as.data.frame(tab)
+tab<-tab[,-c(1:2)]
+col_names<-tab[1,]
